2a4801bf50800486b587bbc612af23efaea6ef1f
destrier2 <zoe.li@student.tdsb.on.ca>
2025-03-04 17:34:33 -0500

    Update ArraysAssignment.java

--- arrays assignment/ArraysAssignment.java
+++ arrays assignment/ArraysAssignment.java
@@ -6,45 +6,74 @@ Description: Practice using and manipulating parallel arrays. This program ranks
 */
 /* 
 TODOs:
-- search results by player
-- sort players by total score/each category
-- Add player in memory
-- Save current data to file
-- Load data from file
-- Help page where we describe what all values are
+- search results by player - Zoe
+- sort players by total score/each category - Jason
+- Add player in memory - Jason
+- Save current data to file - Jason
+- Load data from file - Zoe
+- Help page where we describe what all values are - Zoe
 
 
 - Save current state in file?
-*/ 
+*/
 
 import java.util.*;
+import java.io.File;
 
-public class ArraysAssignment {
-	private String[] names = {"Matt Murl", "Chris Kam", "Gabe Isaac", "Lucy Gao", "Joseph Wang", "Clara Liu", "Veronica Davey-Young", "Chengshu Bi", "Celine Lai", "Hannah McKenna", "Caleb Soroka"};
-	private int[] goals = {19, 0, 2, 6, 12, 6, 7, 15, 10, 10, 5};
-	private int[] assists = {13, 4, 6, 2, 2, 3, 1, 8, 4, 10, 3};
-	private int[] secondAssists = {7, 4, 2, 3, 1, 5, 3, 3, 7, 6, 1};
-	private int[] throwaways = {8, 2, 6, 5, 4, 11, 5, 16, 4, 3, 1};
-	private int[] ds = {13, 0, 2, 4, 4, 5, 4, 8, 5, 4, 2};
+public class ArraysAssignment{
+	
+	Scanner s;
+	private int n;
+	private String[] names;
+	private int[] goals;
+	private int[] assists;
+	private int[] secondAssists;
+	private int[] throwaways;
+	private int[] ds;
 	private double[] ratio;
 	private int[] totalScore;
 	
 	ArraysAssignment() {
-		ratio = new double[names.length];
-		totalScore = new int[names.length];
+		try {
+			File file = new File("stats.txt");
+			s = new Scanner(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void setData() {
-		for(int i = 0; i < names.length; i++) {
-			double ratioValue =  ((double)assists[i]+secondAssists[i])/throwaways[i]; //Calculate the ratio of assists and second assists to the number of throwaways
-			
+	public void readData() {
+		int n = s.nextInt(); //read in the number of players
+		s.nextLine(); //clear buffer
+		
+		//Set the sizes of each of the arrays
+		names = new String[n];
+		goals = new int[n];
+		assists = new int[n];
+		secondAssists = new int[n];
+		throwaways = new int[n];
+		ds = new int[n];
+		ratio = new double[n];
+		totalScore = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			names[i] = s.nextLine();
+			goals[i] = s.nextInt();
+			assists[i] = s.nextInt();
+			secondAssists[i] = s.nextInt();
+			throwaways[i] = s.nextInt();
+			ds[i] = s.nextInt();
+			if (s.hasNextLine()) {
+				s.nextLine(); //Clear buffer
+			}
+			double ratioValue = ((double)assists[i]+secondAssists[i])/throwaways[i]; //Calculate the ratio of assists and second assists to the number of throwaways
 			ratio[i] = Math.round(ratioValue*10)/10.0;
 			totalScore[i] = goals[i]*3 + assists[i]*3 + secondAssists[i] - throwaways[i]*2 + ds[i]*2;
 		}
 	}
 	
-	public void sort(String sortBy) { //Jason's weird sorting thing
-	
+	public void sort(String sortBy) { //Copy the arrays, then sort them. Return sorted arrays (no return type right now because I'm lazy)
+		
 	}
 	
 	public String displayAll() {
@@ -52,7 +81,7 @@ public class ArraysAssignment {
 		text+="--------------------------------------------------------------------------";
 		text+="\n";
 		for(int i = 0; i < names.length; i++) {
-			if (names[i].length() > 20) {	
+			if (names[i].length() > 20) {
 				text+="\t"+names[i].substring(0, 17) +"...\t";
 			} else if (names[i].length() > 17) {
 				text+="\t"+names[i]+"\t";
@@ -69,41 +98,66 @@ public class ArraysAssignment {
 			}
 			text+="|\t"+goals[i]+"\t|\t"+assists[i]+"\t|\t"+secondAssists[i]+"\t|\t"+throwaways[i]+"\t|\t"+ds[i]+"\t|\t"+ratio[i]+"  |\t"+totalScore[i]+"\n";
 		}
+		text+="--------------------------------------------------------------------------";
 		return text;
 	}
 	
 	public static void main(String[] args) {
-		ArraysAssignment a = new ArraysAssignment();
+		ArraysAssignment a = new ArraysAssignment();		
 		Scanner s = new Scanner(System.in);
-		a.setData();
+		a.readData();
+		boolean run = true;
 		
-		System.out.println("This program stores the stats for ultimate frisbee players.\nThere are currently " + a.names.length + " players stored in this system.");
-		boolean takeInput = true;
-		int choice = 0;
-		
-		while(takeInput) {
-			System.out.println("You may:\n\t1. display all\n\t2. display the best player based on a category");
-			String temp = s.nextLine();
-			try {
-				choice = Integer.parseInt(temp);
-				if(choice >= 1 && choice <= 2) {
-					takeInput = false;
-				} else {
-					System.out.println("Please ensure that you enter an integer that corresponds to one of the options.");
+		while (run) {
+			System.out.println("This program stores the stats for ultimate frisbee players.\nThere are currently " + a.names.length + " players stored in this system.");
+			boolean takeInput = true;
+			int choice = 0;
+			
+			while(takeInput) {
+				System.out.println("You may:\n\t1. display all\n\t2. sort players by a category\n\t3. view a player's stats\n\t4. add a player in memory\n\t5. save current data to a file\n\t6. load data from a file\n\t7. visit the help page\n\t8. exit the program");
+				String temp = s.nextLine();
+				try {
+					choice = Integer.parseInt(temp);
+					if (choice >= 1 && choice <= 8) {
+						takeInput = false;
+					} else {
+						System.out.println("Please ensure that you enter an integer that corresponds to one of the options.");
+						System.out.println("Press enter to continue.");
+						s.nextLine();
+					}
+				} catch (Exception e){
+					System.out.println("Please ensure you enter the integer that corresponds to one of the options.");
+					System.out.println("Press enter to continue.");
+					s.nextLine();
 				}
-			} catch (Exception e) {
-				System.out.println("Please ensure you enter the integer that corresponds to one of the options.");
+			}
+			
+			switch (choice) {
+				case 1:
+					System.out.println(a.displayAll());
+					System.out.println("Press enter to return to the main menu.");
+					s.nextLine();
+					break;
+				case 2: 
+					
+					break;
+				case 3: 
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				case 7: 
+					break; 
+				case 8:
+					System.out.println("Thank you for using this program. Goodbye!");
+					System.exit(0);
+					break;
 			}
 		}
-		
-		switch (choice) {
-			case 1:
-				System.out.println(a.displayAll());
-				break;
-			case 2:
-				break;
-		}
+
 		
 	}
-	
 }
