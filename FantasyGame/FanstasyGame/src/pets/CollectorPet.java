4481d18c50abb29e4e2fdcc5d438c5431ad53d37
destrier2 <zoe.li@student.tdsb.on.ca>
2025-04-12 18:25:15 -0400

    Update CollectorPet.java

--- FantasyGame/FanstasyGame/src/pets/CollectorPet.java
+++ FantasyGame/FanstasyGame/src/pets/CollectorPet.java
@@ -18,33 +18,35 @@ public class CollectorPet extends Pet {
 	@Override
 	public void interact(Player player) {
 		Room room = player.getCurrentRoom();
-		int extraGold = rand.nextInt(1+level*2);
-		System.out.println("Your pet " + name + " found an extra " + extraGold + " gold");
-		room.setGoldAmount(room.getGoldAmount()+extraGold);
+		if (room.getClass().getSimpleName().equals("NormalRoom")) {
+			int extraGold = rand.nextInt((1+level)*2) + 1;
+			System.out.println("\nYour pet " + name + " found an extra " + extraGold + " gold");
+			player.addGold(extraGold);
+			totalGold += extraGold;
+		}
+		if (totalGold % 20 == 0) {
+			levelUp();
+		}
 	}
 	
 	@Override
 	public boolean levelUp() {
 		if ((Math.sqrt(totalGold) + 3) / 10 < getLevel()) {
-			//System.out.println("Your pet " + name + " is trying to level up . . .");
 			if (super.levelUp()) {
-				System.out.println("Your pet " + name + " successfully leveled up!");
+				System.out.println("\nYour pet " + name + " successfully leveled up!");
 				return true;
 			} else if (getLevel() == MAX_LEVEL) {
-			//	System.out.println("Your pet " + name + " is already at the highest level!");
 				return false;
 			} else {
-			//	System.out.println("Your pet " + name + " failed to level up.");
 				return false;
 			}
 		} else {
-			//System.out.println("Your pet " + name + " can't level up yet! It hasn't collected enough gold.");
 			return false;
 		}
 	}
 	
 	public String toString() {
-		return name + " (Niffler)";
+		return name + " (Niffler, Lvl " + level +")";
 	}
 	
 }
