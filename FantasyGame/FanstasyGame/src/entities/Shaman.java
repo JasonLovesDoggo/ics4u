b0297269fa190ce0c3194e19ffd75931e21315cb
Jason Cameron <git@jasoncameron.dev>
2025-04-16 14:23:57 -0400

    fix: Refactor LuckyPet and Shaman classes for improved readability and consistency

--- FantasyGame/FanstasyGame/src/entities/Shaman.java
+++ FantasyGame/FanstasyGame/src/entities/Shaman.java
@@ -2,7 +2,8 @@ package entities;
 
 import utilities.Entity;
 
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Shaman implements Entity {
     @Override
@@ -10,13 +11,13 @@ public class Shaman implements Entity {
         Random rng = new Random();
         System.out.println("A mystical shaman offers to increase your gold carrying capacity.");
         System.out.println("The shaman performs a ritual...");
-        if (player.getGoldCapacity() >= Player.GLOBAL_MAX_GOLD_CAPACITY+player.getPetCapacity()) {
+        if (player.getGoldCapacity() >= Player.GLOBAL_MAX_GOLD_CAPACITY + player.getPetCapacity()) {
             System.out.println("The shaman cries out, IT WONT WORK! You are already at maximum capacity!");
             return;
         } else if (rng.nextInt(10) > 5) { // 50% chance to fail and do nothing
             System.out.println("The shaman's magic fails! You feel a strange sensation, but nothing happens.");
             return;
-        } else if (rng.nextInt(50) >= 48 ) { // 4% chance to backfire
+        } else if (rng.nextInt(50) >= 48) { // 4% chance to backfire
             System.out.println("The shaman's magic backfires! You feel a surge of energy, but in the chaos, Sam, who is now a goblin sneaks in and robs you!!!.");
             player.reduceHealth(30); // Reduce health by 30%
             player.removeGold(9999); // Remove all gold
@@ -27,7 +28,7 @@ public class Shaman implements Entity {
         int increase = rng.nextInt(10, 30);
         player.increaseGoldCapacity(increase);
         System.out.println("Your gold carrying capacity has been increased by " + increase + "!\n(Press enter to continue)\n . . .");
-		  Scanner s = new Scanner(System.in);
-		  s.nextLine();
+        Scanner s = new Scanner(System.in);
+        s.nextLine();
     }
 }
--- FantasyGame/FanstasyGame/src/pets/LuckyPet.java
+++ FantasyGame/FanstasyGame/src/pets/LuckyPet.java
@@ -1,32 +1,31 @@
 package pets;
 
-import utilities.Interactable;
 import entities.Player;
-import rooms.*;
-import java.util.Random;
+import rooms.NormalRoom;
+import rooms.Room;
 
 public class LuckyPet extends Pet {
-		
-	public LuckyPet(String name) {
-		super(name);
-	}
-	
-	@Override
-	public void interact(Player player) {
-		Room room = player.getCurrentRoom();
-		if (levelUp() && room instanceof NormalRoom) {
-			if (rand.nextInt(10)%2 == 0) { //Randomly select which chance to change
-				((NormalRoom)room).changeShamanChance((int)level/2);
-				System.out.println("The chance of finding a shaman has been changed!");
-			} else {
-				((NormalRoom)room).changeDoctorChance((int)level/2);
-				System.out.println("The chance of finding a doctor has been changed!");
-			}
-		}
-	}
-	
-	public String toString() {
-		return name + " (Lucky creature, Lvl " + level + ")";
-	}
-	
+
+    public LuckyPet(String name) {
+        super(name);
+    }
+
+    @Override
+    public void interact(Player player) {
+        Room room = player.getCurrentRoom();
+        if (levelUp() && room instanceof NormalRoom) {
+            if (rand.nextInt(10) % 2 == 0) { //Randomly select which chance to change
+                ((NormalRoom) room).changeShamanChance((int) level / 2);
+                System.out.println("The chance of finding a shaman has been changed!");
+            } else {
+                ((NormalRoom) room).changeDoctorChance((int) level / 2);
+                System.out.println("The chance of finding a doctor has been changed!");
+            }
+        }
+    }
+
+    public String toString() {
+        return name + " (Lucky creature, Lvl " + level + ")";
+    }
+
 }
